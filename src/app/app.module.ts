acfcacedc8448fc097a5cf418807b02cf758d754
Kirk Abbott <kirkabbott1@gmail.com>
2017-10-03 18:41:25 -0400

    crm

---
+++ src/app/app.module.ts
@@ -0,0 +1,88 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+// ngfire imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+
+// component imports
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+// service imports
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterGuard } from './guards/register.guard';
+
+import { SettingsService } from './services/settings.service';
+
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'register', component:RegisterComponent, canActivate:[RegisterGuard]},
+  {path:'login', component:LoginComponent},
+  {path:'add-client', component:AddClientComponent, canActivate:[AuthGuard]},
+  {path:'client/:id', component:ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path:'edit-client/:id', component:EditClientComponent, canActivate:[AuthGuard]},
+  {path:'settings', component:SettingsComponent, canActivate:[AuthGuard]},
+  {path:'**', component:PageNotFoundComponent}
+  
+  
+];
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCe5mIj1Tu9Mi3rg7qExf8ozaoSxJ5ulNk",
+    authDomain: "crmpanel-7489c.firebaseapp.com",
+    databaseURL: "https://crmpanel-7489c.firebaseio.com",
+    storageBucket: "crmpanel-7489c.appspot.com",
+    messagingSenderId: "1032552197655"
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule,
+    FlashMessagesModule
+  ],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService,
+    AuthService,
+    AuthGuard,
+    SettingsService,
+    RegisterGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/add-client/add-client.component.ts
@@ -0,0 +1,48 @@
+import { ClientService } from './../../services/client.service';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../../models/Client';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { SettingsService } from '../../services/settings.service';
+@Component({
+  selector: 'app-add-client',
+  templateUrl: './add-client.component.html',
+  styleUrls: ['./add-client.component.css']
+})
+export class AddClientComponent implements OnInit {
+  client:Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  }
+  disableBalanceOnAdd:boolean = false;
+
+  constructor(
+    public flashMessagesService:FlashMessagesService,
+    public router:Router,
+    public clientService:ClientService,
+    public settingsService:SettingsService
+  ) { }
+
+  ngOnInit() {
+    this.disableBalanceOnAdd = this.settingsService.getSettings().disableBalanceOnAdd;
+  }
+
+  onSubmit({value, valid}:{value:Client, valid:boolean}){
+    if(this.disableBalanceOnAdd){
+      value.balance = 0;
+    }
+    if(!valid){
+      this.flashMessagesService.show('Please complete form', {cssClass:'alert-danger', timeout: 3000});
+      this.router.navigate(['add-client']);
+      
+    } else {
+      // 
+      this.clientService.newClient(value);
+      this.flashMessagesService.show('New Client added', {cssClass:'alert-success', timeout: 3000});
+      this.router.navigate(['/']);
+    }
+  }
+}
